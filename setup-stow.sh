24d1c0f705e061895d5207dfa26b3ba16a433df2
Kyle Grimsrud-Manz <hi@kylegrimsrudma.nz>
2024-01-16 11:50:11 -0500

    fix code-insiders

--- setup-stow.sh
+++ setup-stow.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 stow -t "$HOME" xdg-home
-
 if [ -d "$HOME/.config/Code - Insiders" ]; then
-    stow -t "$HOME/.config/Code - Insiders" code
+    stow -t "$HOME/.config/Code - Insiders" -d xdg-home/.config Code
 fi
