b949fea18f9bd2a3e4f61b2e02ed1c5add665bd1
Kyle Grimsrud-Manz <hi@kylegrimsrudma.nz>
2025-01-07 09:02:31 -0800

    accommodate system files

--- setup-stow.sh
+++ setup-stow.sh
@@ -4,3 +4,10 @@ stow -t "$HOME" xdg-home
 if [ -d "$HOME/.config/Code - Insiders" ]; then
     stow -t "$HOME/.config/Code - Insiders" -d xdg-home/.config Code
 fi
+
+for dir in */; do
+    dir=${dir%/}  # Remove trailing slash
+    if [[ $dir != "xdg-home" ]]; then
+        stow -t "/$dir" "$dir"
+    fi
+done
